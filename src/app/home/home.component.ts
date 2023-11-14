2e9fa14427821ac659067a93e0368628eda5b963
spushkaraj <spushkaraj.dev@gmail.com>
2023-11-14 10:37:21 +0530

    displaying users in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users/users.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  public users: any[];
+
+  constructor(private usersService: UsersService){ 
+    this.users = [];
+  }
+
+  ngOnInit(): void {
+    this.usersService.all().subscribe( res => {
+      this.users = res;
+    });
+  }
 }
